9e3c2c14faac6b9d4ce9f3e3f83a98e9f9222e44
callam <mcg.callam@icloud.com>
2024-03-11 13:35:29 +0000

    ideas router

---
+++ server/ideas.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId } = require('./db');
+const ideasRouter = express.Router();
+
+ideasRouter.use('/:ideaId', (req, res, next) => {
+    const id = req.params.ideaId
+    if(isNaN(id) || !getFromDatabaseById('ideas', id)){
+        res.sendStatus(404)
+    }else{
+        next()
+    }
+})
+
+// GET /api/ideas to get an array of all ideas.
+ideasRouter.get('/', (req, res, next)=>{
+    res.send(getAllFromDatabase("ideas"))
+})
+
+// POST /api/ideas to create a new idea and save it to the database.
+ideasRouter.post('/', (req, res, next)=> {
+    const newIdea = req.body;
+    const addedIdea = addToDatabase('ideas', newIdea)
+
+    if(!addedIdea){
+        res.sendStatus(500)
+    }else{
+        res.status(201).send(addedIdea)
+    }
+})
+
+// GET /api/ideas/:ideaId to get a single idea by id.
+ideasRouter.get('/:ideaId', (req, res, next)=>{
+    const id = req.params.ideaId
+    const idea = getFromDatabaseById("ideas", id)
+    res.send(idea)
+})
+
+// PUT /api/ideas/:ideaId to update a single idea by id.
+ideasRouter.put('/:ideaId', (req, res, next)=>{
+    const id = req.params.ideaId
+    const {name, description, numWeeks, weeklyRevenue } = req.body
+    const newidea = {id, name, description, numWeeks, weeklyRevenue }
+    updateInstanceInDatabase('ideas', newidea)
+    res.send(newidea)
+})
+
+// DELETE /api/ideas/:ideaId to delete a single idea by id.
+ideasRouter.delete('/:ideaId', (req, res, next)=> {
+    const id = req.params.ideaId
+    deleteFromDatabasebyId('ideas', id)
+    res.sendStatus(204)
+})
+
+module.exports = ideasRouter;
